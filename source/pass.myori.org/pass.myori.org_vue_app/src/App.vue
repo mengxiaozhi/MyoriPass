f2449e77a1338a811151a8a6942f74b11fecb34b
Angelo0218 <maplech0218@gmail.com>
2023-10-24 16:01:05 +0800

    app測試

---
+++ source/pass.myori.org/pass.myori.org_vue_app/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import Header_vue from './components/header.vue'
+export default {
+  components: {
+    Header_vue,
+  }
+}
+</script>
+
+<template>
+  <Header_vue />
+  <section>
+    <div id="page-wrapper">
+      <div style="padding-top: 100px;">
+        <router-view />
+      </div>
+    </div>
+  </section>
+</template>
