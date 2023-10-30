a5828ba4de498cfc4af92318b892e6456839f10a
Mengxiaozhi <>
2023-10-30 20:48:47 +0800

    Hey,幫我做Breach

---
+++ application/Web/1.3/pass.myori.org_vue/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    proxy: {
+      '/api': 'http://localhost' 
+    }
+  },
+  resolve: {
+    alias: {
+      // 设置 '@' 指向 'src' 目录
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+})
