56d91cc074e5a1690b1fbca7d1e04772f80624c0
Mengxiaozhi <>
2023-12-09 18:34:04 +0800

    倒计时

--- README.md
+++ README.md
@@ -1,23 +1,24 @@
 # 苗栗通MyoriPass
 地址：https://pass.myori.org
+
 ## 簡介
 這是一個架空國家的出入國申報系統專案，提供用戶登入/註冊、帳號管理、出入境申報、QrCode自動通關等功能（按照GDPR規則設計）。<br>
 其實是一個很好的新人學習專案，或者用來變成健康碼？？？會員管理系統之類的東西。<br>
 
-## 安裝
+## 構建
 ```bash
 git clone https://github.com/mengxiaozhi/MyoriPass.git
-cd MyoriPass/pass.myroi.org/pass.myori.org_vue
+cd /pass.myroi.org/pass.myori.org_vue
 npm install  
 npm run build #構建
 ```
 把構建出來的Dist資料夾丟到服務器<br>
-MyoriPass/pass.myroi.org/pass.myori.org_vue.php_api<br>
+/pass.myroi.org/pass.myori.org_vue.php_api<br>
 這個路徑下的所有東西則丟到Dist根目錄的 /Api 資料夾下。
 
 ## 貢獻
-萌小志Mengxiaozhi
-Angelo0218
+萌小志Mengxiaozhi<br>
+Angelo0218<br>
 感謝所有為這個專案做出貢獻的開發者和貢獻者。
 
 ## 授權
--- source/pass.myori.org/pass.myori.org_vue/src/page/user/user.vue
+++ source/pass.myori.org/pass.myori.org_vue/src/page/user/user.vue
@@ -3,9 +3,9 @@ import { ref, onMounted, computed } from 'vue';
 import axios from 'axios';
 import { useRouter } from 'vue-router';
 import { useUserStore } from '@/store/userStore';
+
 export default {
   setup() {
-
     // 用戶狀態管理
     const userStore = useUserStore();
     const qrCodeUrl = ref('');
@@ -13,6 +13,11 @@ export default {
     const displayedName = ref('');
     const greeting = ref('');
     const router = useRouter();
+
+    // 倒數計時器
+    const countdown = ref(15);
+
+    // Function to fetch user data
     const fetchUserData = () => {
       axios.get('/api/user.php')
         .then(response => {
@@ -22,8 +27,10 @@ export default {
             displayedName.value = response.data.displayedName;
             greeting.value = response.data.greeting;
             userStore.setStatus(1);
-          } else {
 
+            // 重置倒數計時器
+            countdown.value = 15;
+          } else {
             router.push('/');
           }
         })
@@ -32,6 +39,12 @@ export default {
           userStore.setStatus(0); // 更新用戶狀態
         });
     };
+
+    // Function to fetch user data every 15 seconds
+    const refreshUserData = () => {
+      fetchUserData();
+    };
+
     // qrcode
     const qrCodeImageUrl = computed(() => {
       if (qrCodeUrl.value) {
@@ -40,33 +53,49 @@ export default {
       return '';
     });
 
+    // Call fetchUserData on component mount
+    onMounted(() => {
+      fetchUserData();
 
-    onMounted(fetchUserData);
+      // Refresh user data every 15 seconds
+      setInterval(refreshUserData, 15000);
 
+      // Update countdown every second
+      setInterval(() => {
+        countdown.value = Math.max(countdown.value - 1, 0);
+      }, 1000);
+    });
 
     return {
       qrCodeUrl,
       countries,
       displayedName,
       greeting,
-      qrCodeImageUrl
+      qrCodeImageUrl,
+      countdown
     };
   },
 };
 </script>
+
 <template>
-  <h1>
-    {{ displayedName }} ， {{ greeting }}<br>
-    歡迎回來
-  </h1>
-  <h3 class="title-section">個人識別碼 QR-ID</h3>
-  <div style="display:flex;justify-content:center;">
-    <img :src="qrCodeImageUrl" alt="QR-ID">
-  </div>
-  <p style="display:flex;justify-content:center;">國籍：{{ countries }}</p>
-  <h3 class="title-section">身分授權紀錄</h3>
-  <div class="a_list">
-    <h4 style="display: flex; justify-content: center;">無授權/出入國紀錄</h4>
+  <div>
+    <h1>
+      {{ displayedName }} ， {{ greeting }}<br>
+      歡迎回來
+    </h1>
+    <h3 class="title-section">個人識別碼 QR-ID</h3>
+    <div style="display:flex;justify-content:center;">
+      <img :src="qrCodeImageUrl" alt="QR-ID">
+    </div>
+    <div style="display:flex;justify-content:center;margin-top:-50px;">
+        <p style="color: rgb(255, 255, 255); background-color: rgb(0, 23, 193); min-width: 225px; height: 27px; display: flex; justify-content: center;">{{ countdown }} 秒後刷新</p>
+    </div>
+    <p style="display:flex;justify-content:center;">國籍：{{ countries }}</p>
+    <h3 class="title-section">身分授權紀錄</h3>
+    <div class="a_list">
+      <h4 style="display: flex; justify-content: center;">無授權/出入國紀錄</h4>
+    </div>
   </div>
 </template>
 
@@ -77,5 +106,5 @@ export default {
   background-color: #4144501c;
   border-radius: 11px;
   padding: 10px;
-  }
+}
 </style>
