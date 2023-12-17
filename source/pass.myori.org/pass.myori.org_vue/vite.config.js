7e2d7fce3e0689531de7d37fce8814d3afb78140
Angelo0218 <maplech0218@gmail.com>
2023-12-17 14:40:08 +0800

    reader.vue數據綁定修正 ui待調整

--- source/pass.myori.org/pass.myori.org_vue/vite.config.js
+++ source/pass.myori.org/pass.myori.org_vue/vite.config.js
@@ -1,24 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import mkcert from "vite-plugin-mkcert";
+// import mkcert from "vite-plugin-mkcert";
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    mkcert()
+    // mkcert()
   ],
   server: {
     host: '0.0.0.0',
     https: true,
     proxy: {
-      '/api': {
-        target: 'https://pass.myori.org/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+      // '/api': {
+      //   target: 'https://pass.myori.org/api',
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/api/, ''),
+      // },
     },
   },
   resolve: {
